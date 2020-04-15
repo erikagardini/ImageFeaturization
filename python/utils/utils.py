24dac55356f4c2d1fcf34bb34d9deeea51087b62
erikagardini <32106127+erikagardini@users.noreply.github.com>
2020-04-15 13:41:46 +0200

    Add files via upload

---
+++ python/utils/utils.py
@@ -0,0 +1,53 @@
+#References:
+#[1] Lecoutre, A., Negrevergne, B., and Yger, F.
+    # Rasta: Recognizing art style automatically in painting with deep learning.
+        #In Asian Conference on Machine Learning, 2017.
+#[2] https://github.com/bnegreve/rasta
+
+import os
+from os.path import join
+
+#PREPROCESSING FUNCTIONS
+
+def imagenet_preprocess_input(x):
+    # 'RGB'->'BGR'
+    x = x[:, :, ::-1]
+    # Zero-center by mean pixel
+    x[:, :, 0] -= 103.939
+    x[:, :, 1] -= 116.779
+    x[:, :, 2] -= 123.68
+    return x
+
+def wp_preprocess_input(x):
+    # 'RGB'->'BGR'
+    x = x[:, :, ::-1]
+
+    x[:,:,0] -=  133.104
+    x[:,:,0] -=  119.973
+    x[:,:,0] -=  104.432
+
+    return x
+
+def custom_preprocess_input(x):
+    x = x[:, :, ::-1]
+    # Zero-center by mean pixel
+    x[:, :, 0] -= 103.939
+    x[:, :, 1] -= 116.779
+    x[:, :, 2] -= 123.68
+    x *= 1./255
+
+
+#DICO FUNCTIONS
+
+def get_dico():
+    classes = []
+    PATH = os.path.dirname(__file__)
+    directory = join(PATH,'../../datasets/wikipaintings_full/wikipaintings_train')
+    for subdir in sorted(os.listdir(directory)):
+        if os.path.isdir(os.path.join(directory, subdir)):
+            classes.append(subdir)
+    class_indices = dict(zip(classes, range(len(classes))))
+    return class_indices
+
+def invert_dico(dico):
+    return {v: k for k, v in dico.items()}
